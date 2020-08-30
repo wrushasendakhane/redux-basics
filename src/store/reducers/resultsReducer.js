6e056faa2eb2a1e02062588e3780f71f95c5a19e
Wrushasen Dakhane <wrushu2004@gmail.com>
2020-08-30 10:53:20 -0700

    Upgraded to use multiple reducers

---
+++ src/store/reducers/resultsReducer.js
@@ -0,0 +1,25 @@
+import * as actionTypes from "../actions"
+
+const initialState = {
+  results: []
+}
+
+const resultsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: action.result })
+      }
+    case actionTypes.DELETE_RESULT:
+      return {
+        ...state,
+        results: state.results.filter(result => result.id !== action.id)
+      }
+    default:
+      break;
+  }
+  return state;
+}
+
+export default resultsReducer;
